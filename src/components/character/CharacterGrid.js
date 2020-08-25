988ff33b7ffc15a61f6bf2f0bc9c13e8d90b491c
Kibotot <KibototV2@gmail.com>
2020-08-25 16:19:15 +0200

    first commit

---
+++ src/components/character/CharacterGrid.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CharacterItem from "./CharacterItem";
+
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? (
+    <h1>loading...</h1>
+  ) : (
+    <section className="cards">
+      {items.map((item) => (
+        <CharacterItem item={item} key={item.char_id} />
+      ))}
+    </section>
+  );
+};
+
+export default CharacterGrid;
